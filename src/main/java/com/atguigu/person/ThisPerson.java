ed3e6cf6d123139b6b8244d5bb41aec2e44cb123
lhuc@atguigu <lhuc@atguigu.com>
2017-08-23 19:29:51 +0800

    555555555

--- src/main/java/com/atguigu/person/ThisPerson.java
+++ src/main/java/com/atguigu/person/ThisPerson.java
@@ -10,5 +10,6 @@ public class ThisPerson {
 		System.out.println("我是你的一阵风");
 		System.out.println("77777777777777");
 		System.out.println("555555555555555555555");
+		System.out.println("对酒当歌，人生几何！");
 	}
 }
